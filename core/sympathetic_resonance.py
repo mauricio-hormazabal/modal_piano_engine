b5b521edeb3317d0233b08938f65cda41c29c1ac
mauricio-hormazabal <mhormaza@gmail.com>
2025-07-13 04:23:16 -0400

    Update sympathetic_resonance.py

--- core/sympathetic_resonance.py
+++ core/sympathetic_resonance.py
@@ -28,12 +28,11 @@ def generate_sympathetic_response(fs, midi_note, velocity, active_notes,
 
     # Cuerdas libres: todas menos las activas
     all_notes = list(range(21, 109))
-    #notes_actives = [note for note in active_notes]
+   
     free_notes = [n for n in all_notes if n not in active_notes]
 
     # Buscar solo las cuerdas libres relevantes
-    #resonant_notes = find_resonant_free_notes(active_notes, free_notes,
-    #                                          harmonic_range, tolerance)
+  
     resonant_notes = find_resonant_free_notes(midi_note, free_notes,
                                             harmonic_range, tolerance)
 
@@ -53,19 +52,3 @@ def generate_sympathetic_response(fs, midi_note, velocity, active_notes,
     envelope = attack_envelope(fs, duration, velocity, fade_min=0.008, fade_max=0.02 ) #Valores de prueba, ajustar
     response *= envelope
     return response
-
-"""
-def OLD_find_resonant_free_notes(active_notes, free_notes, harmonic_range=10, tolerance=0.005): #tolerance 0.02
-
-    resonant_notes = set()
-    for midi_note in active_notes:
-        f0 = midi_to_freq(midi_note)
-        for n in range(1, harmonic_range + 1):
-            f_harm = f0 * n
-            for free_note in free_notes:
-                f_free = midi_to_freq(free_note)
-                rel_diff = abs(f_free - f_harm) / f_harm
-                if rel_diff < tolerance:
-                    resonant_notes.add(free_note)
-    return sorted(resonant_notes)
-"""
